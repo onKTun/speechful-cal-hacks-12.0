1ec06aecb8d4b8864e0337aad757654f32608ba7
onKTun <kevinkltun@gmail.com>
2025-10-26 02:16:00 -0700

    basic transcript tracking

--- client/src/learning/LearningPage.tsx
+++ client/src/learning/LearningPage.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useRef, useState } from "react"
+import { processTranscript, compareTextAccuracy } from "./utils/processTranscript"
 
 export default function LearningPageDev() {
     const [isMicOpen, setMicStatus] = useState(false)
     const [output, setOutput] = useState("")
     const microphone = useRef<MediaRecorder | undefined>(undefined)
     const listenButton = useRef<HTMLButtonElement | null>(null)
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const originalLines = processTranscript("I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.\nFive score years ago a great American in whose symbolic shadow we stand today signed the Emancipation Proclamation. "
+        , 1)
+    const threshold = 0.9
 
     const openMicrophone = async (microphone: MediaRecorder, socket: WebSocket) => {
         return new Promise((resolve: any) => {
@@ -35,6 +41,25 @@ export default function LearningPageDev() {
         microphone.stop();
         setMicStatus(false)
     }
+//logic to see where the user is in the text
+    useEffect(() => {
+        const processOutput = (text: string) => {
+            if (text == "") {
+                return;
+            }
+
+            const words = processTranscript(text, 1)
+            const transcriptWords = originalLines.slice(currentIndex, currentIndex + words.length)
+
+            const score = compareTextAccuracy(words.join(' '), transcriptWords.join(' '))
+
+            if (score >= threshold) {
+                setCurrentIndex(currentIndex + words.length)
+            }
+        }
+
+        processOutput(output)
+    }, [output])
 
     useEffect(() => {
         const socket = new WebSocket("ws://localhost:3000");
@@ -99,10 +124,22 @@ export default function LearningPageDev() {
 
     return (
         <div>
+            <ul>
+                {originalLines.map((line, idx) => (
+                    <li key={idx} style={{ display: "flex", gap: 8 }}>
+                        <span style={{ minWidth: 24, textAlign: "right", color: "#666" }}>{idx}.</span>
+                        <span>{line}</span>
+                    </li>
+                ))}
+            </ul>
+            <div>{currentIndex}</div>
             <button ref={listenButton}>
                 Click to toggle listening. Status: {isMicOpen + ""}
             </button>
-            <div>{output}</div>
+            <h4>Similarity</h4>
+            <div>Original {originalLines.slice(currentIndex, currentIndex + processTranscript(output, 1).length).join(" ")}</div>
+            <div>Transcribed {output}</div>
+            <div>Score: {compareTextAccuracy(originalLines[currentIndex], output)}</div>
         </div>
     )
 }
